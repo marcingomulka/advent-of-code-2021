21c474b423f26efcf950cc778e8f2fcb256c06d9
Marcin Gomulka <magomulka@gmail.com>
2021-12-10 07:02:41 +0100

    refactor

--- Day10/src/org/example/advent/day10/Main2.java
+++ Day10/src/org/example/advent/day10/Main2.java
@@ -31,37 +31,40 @@ public class Main2 {
             LinkedList<Character> stack = new LinkedList<>();
 
             List<Integer> chars = line.chars().boxed().collect(Collectors.toList());
-            boolean skipLine = false;
-            for (Integer charValue : chars) {
-                char sign = (char) charValue.intValue();
-                if (chunkPairs.containsKey(sign)) {
-                    stack.push(sign);
-                } else {
-                    char shouldMatch = chunkPairs.get(stack.pop());
-                    if (sign != shouldMatch) {
-                        skipLine = true;
-                        break;
-                    }
-                }
-            }
-            if (skipLine) {
+            if (!validateLine(stack, chars)) {
                 continue;
             }
-            long linePoints = 0L;
-            while (!stack.isEmpty()) {
-                char missingMatch = stack.pop();
-                char missing = chunkPairs.get(missingMatch);
-                //System.out.print(missing);
-                long points = chunkPoints.get(missing);
-                linePoints = linePoints * 5L + points;
-            }
-            //System.out.println();
-            //System.out.println(linePoints);
-            scores.add(linePoints);
+            scores.add(calculatesPoints(stack));
         }
         Collections.sort(scores);
-
         System.out.println(scores.get(scores.size() / 2));
+    }
+
+    private static long calculatesPoints(LinkedList<Character> stack) {
+        long linePoints = 0L;
+        while (!stack.isEmpty()) {
+            char missing = chunkPairs.get(stack.pop());
+            //System.out.print(missing);
+            long points = chunkPoints.get(missing);
+            linePoints = linePoints * 5L + points;
+        }
+        //System.out.println();
+        return linePoints;
+    }
+
+    private static boolean validateLine(LinkedList<Character> stack, List<Integer> chars) {
+        for (Integer charValue : chars) {
+            char sign = (char) charValue.intValue();
+            if (chunkPairs.containsKey(sign)) {
+                stack.push(sign);
+            } else {
+                char shouldMatch = chunkPairs.get(stack.pop());
+                if (sign != shouldMatch) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     private static List<String> readInput() throws IOException {
