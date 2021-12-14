23354150e7c113e27f72f94615953067653e39ec
Marcin Gomulka <magomulka@gmail.com>
2021-12-14 08:24:17 +0100

    solution for day 14

---
+++ Day14/src/org/example/advent/day14/Main.java
@@ -0,0 +1,79 @@
+package org.example.advent.day14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = readInput();
+
+        Map<String, String> rules = new HashMap<>();
+        String template = lines.get(0).trim();
+
+        Map<String, Long> chunkOccurrences = new HashMap<>();
+        for (int i = 0; i < template.length() - 1; i++) {
+            String chunk = template.substring(i, i + 2);
+
+            chunkOccurrences.compute(chunk, (k, v) -> v != null ? v + 1 : 1);
+        }
+        Map<String, Long> letterOccurrences = template.chars()
+                .boxed()
+                .collect(Collectors.groupingBy(Character::toString, Collectors.counting()));
+
+        for (String line : lines) {
+            if (line.contains("->")) {
+                String[] chunks = line.split("->");
+                String key = chunks[0].trim();
+                String value = chunks[1].trim();
+
+                rules.put(key, value);
+            }
+        }
+
+        for (int cycle = 0; cycle < 40; cycle++) {
+            Map<String, Long> previous = chunkOccurrences.entrySet().stream()
+                    .filter(entry -> entry.getValue() > 0)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+            for (String chunk : previous.keySet()) {
+                String toReplace = rules.get(chunk);
+                if (toReplace != null) {
+                    String newChunk = chunk.substring(0, 1) + toReplace;
+                    String newChunk2 = toReplace + chunk.substring(1, 2);
+
+                    long multiplyFactor = previous.get(chunk);
+                    letterOccurrences.compute(toReplace, (k, v) -> v != null ? v + multiplyFactor : multiplyFactor);
+                    chunkOccurrences.compute(newChunk, (k, v) -> v != null ? v + multiplyFactor : multiplyFactor);
+                    chunkOccurrences.compute(newChunk2, (k, v) -> v != null ? v + multiplyFactor : multiplyFactor);
+                    chunkOccurrences.compute(chunk, (k, v) -> v != null ? v - multiplyFactor : multiplyFactor);
+                }
+            }
+            if (cycle == 9) {
+                long max = letterOccurrences.values().stream().mapToLong(val -> val).max().orElse(0L);
+                long min = letterOccurrences.values().stream().mapToLong(val -> val).min().orElse(0L);
+
+                System.out.println("Part1: " + (max - min));
+            }
+        }
+        long max = letterOccurrences.values().stream().mapToLong(val -> val).max().orElse(0L);
+        long min = letterOccurrences.values().stream().mapToLong(val -> val).min().orElse(0L);
+
+        System.out.println("Part2: " + (max - min));
+    }
+
+
+    private static List<String> readInput() throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            lines = reader.lines().collect(Collectors.toList());
+        }
+        return lines;
+    }
+}
