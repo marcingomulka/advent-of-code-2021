4c30a575d16c6e75f56b49e25db4e085ffa1f05b
Marcin Gomulka <magomulka@gmail.com>
2021-12-23 18:46:25 +0100

    refactor, brute force solving both tasks: test.txt and input.txt under 5 minutes

--- Day23/src/org/example/advent/day23/Main.java
+++ Day23/src/org/example/advent/day23/Main.java
@@ -11,31 +11,29 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-    private static long cacheHit = 0L;
-
-    private static Map<Character, Integer> energyCost = Map.of(
+    private static final Map<Character, Integer> ENERGY_COSTS = Map.of(
             'A', 1,
             'B', 10,
             'C', 100,
             'D', 1000
     );
 
-    private static List<Integer> roomPossibleMoves = List.of(0, 1, 3, 5, 7, 9, 10);
+    private static final List<Integer> HALLWAY_POSITIONS = List.of(0, 1, 3, 5, 7, 9, 10);
 
-    private static Map<Integer, Integer> roomExits = Map.of(
+    private static Map<Integer, Integer> ROOM_EXITS = Map.of(
             0, 2,
             1, 4,
             2, 6,
             3, 8
     );
 
-    private static Map<Character, Integer> targetRoom = Map.of(
+    private static Map<Character, Integer> LETTER_TO_ROOM = Map.of(
             'A', 0,
             'B', 1,
             'C', 2,
             'D', 3
     );
-    private static Map<Integer, Character> roomLetters = Map.of(
+    private static Map<Integer, Character> ROOM_TO_LETTER = Map.of(
             0, 'A',
             1, 'B',
             2, 'C',
@@ -79,23 +77,20 @@ public class Main {
         Instant beginTime = Instant.now();
         PriorityQueue<Long> costs = new PriorityQueue<>();
         costs.add(Long.MAX_VALUE);
-        long minCost = trySolve(hallway, roomsPart1, 0L, costs, new HashMap<>());
+        long minCost = trySolve(hallway, roomsPart1, 0L, costs);
 
-        System.out.println("Cache hits: " + cacheHit);
         System.out.println("Partial solutions: " + costs);
         System.out.println("Part1: " + minCost);
         System.out.println("Time: " + Duration.between(beginTime, Instant.now()).toSeconds() + " s");
 
         System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        cacheHit = 0L;
         Arrays.fill(hallway, '.');
 
         beginTime = Instant.now();
         costs = new PriorityQueue<>();
         costs.add(Long.MAX_VALUE);
-        long minCostPart2 = trySolve(hallway, roomsPart2, 0L, costs, new HashMap<>());
+        long minCostPart2 = trySolve(hallway, roomsPart2, 0L, costs);
 
-        System.out.println("Cache hits: " + cacheHit);
         System.out.println("Partial solutions: " + costs);
         System.out.println("Part2: " + minCostPart2);
         System.out.println("Time: " + Duration.between(beginTime, Instant.now()).toSeconds() + " s");
@@ -103,13 +98,7 @@ public class Main {
         System.out.println("HURRRAY, IT FINISHED!!!!!!!!!!!!!!!!!11111");
     }
 
-    private static long trySolve(int[] hallway, List<int[]> rooms, long currentCost, PriorityQueue<Long> costs, Map<Key, Long> cache) {
-        Key key = new Key(hallway, rooms);
-        Long cached = cache.get(key);
-        if (cached != null) {
-            cacheHit++;
-            return cached;
-        }
+    private static long trySolve(int[] hallway, List<int[]> rooms, long currentCost, PriorityQueue<Long> costs) {
         if (currentCost >= costs.peek()) {
             return Long.MAX_VALUE;
         }
@@ -120,31 +109,23 @@ public class Main {
         long minCost = costs.peek();
         boolean moved = false;
         for (int roomId = 0; roomId < rooms.size(); roomId++) {
-            for (int hallwayPos : roomPossibleMoves) {
+            for (int hallwayPos : HALLWAY_POSITIONS) {
                 for (int turn = 0; turn < 2; turn++) {
                     if (turn == 0) {
                         if (canMoveOut(roomId, hallwayPos, hallway, rooms)) {
                             int cost = moveOut(roomId, hallwayPos, hallway, rooms);
-                            //System.out.println("current cost: " + (currentCost + cost));
-                            //printBoard(hallway, rooms);
 
-                            long moveCost = trySolve(hallway, rooms, currentCost + cost, costs, cache);
+                            long moveCost = trySolve(hallway, rooms, currentCost + cost, costs);
                             if (moveCost < minCost) {
                                 minCost = moveCost;
                             }
-
                             moveIn(roomId, hallwayPos, hallway, rooms);
-
                             moved = true;
-                            //printBoard(hallway, rooms);
                         }
                     } else {
                         if (canMoveIn(roomId, hallwayPos, hallway, rooms)) {
                             int cost = moveIn(roomId, hallwayPos, hallway, rooms);
-                            //System.out.println("current cost: " + (currentCost + cost));
-                            //printBoard(hallway, rooms);
-
-                            long moveCost = trySolve(hallway, rooms, currentCost + cost, costs, cache);
+                            long moveCost = trySolve(hallway, rooms, currentCost + cost, costs);
                             if (moveCost < minCost) {
                                 minCost = moveCost;
                             }
@@ -157,15 +138,12 @@ public class Main {
         }
         if (moved) {
             if (minCost < costs.peek()) {
-                cache.put(key, minCost);
                 costs.add(minCost);
             }
             return minCost;
         } else {
-            cache.put(key, Long.MAX_VALUE);
             return Long.MAX_VALUE;
         }
-
     }
 
     private static String arrayToString(int[] array) {
@@ -198,10 +176,10 @@ public class Main {
         if (hallway[hallwaypos] != '.') {
             return false;
         }
-        int roomExit = roomExits.get(roomId);
+        int roomExit = ROOM_EXITS.get(roomId);
         if (hallwaypos == roomExit) return false;
 
-        int roomLetter = roomLetters.get(roomId);
+        int roomLetter = ROOM_TO_LETTER.get(roomId);
         int[] room = rooms.get(roomId);
         if (isRoomEmptyOrSettled(room, roomLetter)) {
             return false;
@@ -217,14 +195,14 @@ public class Main {
             return false;
         }
         int letter = hallway[hallwaypos];
-        if (targetRoom.get((char) letter) != roomId) {
+        if (LETTER_TO_ROOM.get((char) letter) != roomId) {
             return false;
         }
         int[] room = rooms.get(roomId);
         if (!isRoomEmptyOrSettled(room, letter)) {
             return false;
         }
-        int roomExit = roomExits.get(roomId);
+        int roomExit = ROOM_EXITS.get(roomId);
 
         int begin = roomExit < hallwaypos ? roomExit : hallwaypos + 1;
         int end = roomExit > hallwaypos ? roomExit : hallwaypos - 1;
@@ -240,11 +218,11 @@ public class Main {
         int moves = roomPos + 1;
         room[roomPos] = '.';
 
-        int roomExit = roomExits.get(roomId);
+        int roomExit = ROOM_EXITS.get(roomId);
         hallway[hallwaypos] = toMove;
         moves += Math.abs(hallwaypos - roomExit);
 
-        int stepCost = energyCost.get((char) toMove);
+        int stepCost = ENERGY_COSTS.get((char) toMove);
         return stepCost * moves;
     }
 
@@ -253,7 +231,7 @@ public class Main {
         int moves = 0;
 
         int toMove = hallway[hallwaypos];
-        int roomExit = roomExits.get(roomId);
+        int roomExit = ROOM_EXITS.get(roomId);
         moves += Math.abs(hallwaypos - roomExit);
         hallway[hallwaypos] = '.';
 
@@ -261,7 +239,7 @@ public class Main {
         room[roomPos] = toMove;
         moves += roomPos + 1;
 
-        int stepCost = energyCost.get((char) toMove);
+        int stepCost = ENERGY_COSTS.get((char) toMove);
         return stepCost * moves;
     }
 
@@ -304,45 +282,5 @@ public class Main {
             lines = reader.lines().collect(Collectors.toList());
         }
         return lines;
-    }
-}
-
-class Key {
-
-    private final int[] hallway;
-    private final int[] room0;
-    private final int[] room1;
-    private final int[] room2;
-    private final int[] room3;
-
-
-    Key(int[] hallway, List<int[]> rooms) {
-        this.hallway = Arrays.copyOf(hallway, hallway.length);
-        this.room0 = Arrays.copyOf(rooms.get(0), rooms.get(0).length);
-        this.room1 = Arrays.copyOf(rooms.get(1), rooms.get(1).length);
-        this.room2 = Arrays.copyOf(rooms.get(2), rooms.get(2).length);
-        this.room3 = Arrays.copyOf(rooms.get(3), rooms.get(3).length);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Key key = (Key) o;
-        return Arrays.equals(hallway, key.hallway) &&
-                Arrays.equals(room0, key.room0) &&
-                Arrays.equals(room1, key.room1) &&
-                Arrays.equals(room2, key.room2) &&
-                Arrays.equals(room3, key.room3);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Arrays.hashCode(hallway);
-        result = 31 * result + Arrays.hashCode(room0);
-        result = 31 * result + Arrays.hashCode(room1);
-        result = 31 * result + Arrays.hashCode(room2);
-        result = 31 * result + Arrays.hashCode(room3);
-        return result;
     }
 }
