5c0d64e780c8821f2306fd6fa49b808f832d9c57
Marcin Gomulka <magomulka@gmail.com>
2021-12-24 15:21:24 +0100

    solution for day 24

--- Day24/src/org/example/advent/day24/Main.java
+++ Day24/src/org/example/advent/day24/Main.java
@@ -1,202 +1,95 @@
 package org.example.advent.day24;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        List<String> lines = readInput();
+        //All that pseudo assembler input code reduces to the formula() code
+        // with the following params lists per input value:
+        int[] param1 = {1, 1, 1, 1, 26, 1, 1, 26, 1, 26, 26, 26, 26, 26};
+        int[] param2 = {14, 13, 15, 13, -2, 10, 13, -15, 11, -9, -9, -7, -4, -6};
+        int[] param3 = {0, 12, 14, 0, 3, 15, 11, 12, 1, 12, 3, 10, 14, 12};
 
-        long[] registers = new long[4];
+        List<Integer> descendingInput = IntStream.rangeClosed(1, 9)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
 
-        int[] number = new int[14];
-        Arrays.fill(number, 9);
+        List<Integer> solution = new ArrayList<>();
+        Set<Key> pruned = new HashSet<>();
+        trySolve(descendingInput, 0, solution, 0, param1, param2, param3, pruned);
 
-        boolean success = false;
-        while (!success) {
-            int inputIndex = 0;
-            for (String line : lines) {
+        Collections.reverse(solution);
+        System.out.println("Part1: " + solution.stream().map(String::valueOf).collect(Collectors.joining()));
 
-                //System.out.print(Arrays.stream(registers).mapToObj(String::valueOf).collect(Collectors.joining(",")));
-                //System.out.println(" -> " + line);
-                String[] chunks = line.split(" ");
-                String instruction = chunks[0].trim();
-                String arg1 = chunks[1].trim();
-                switch (instruction) {
-                    case "inp":
-                        inp(registers, number, inputIndex, arg1);
-                        inputIndex++;
-                        break;
-                    case "add":
-                        add(registers, arg1, chunks[2]);
-                        break;
-                    case "mul":
-                        mul(registers, arg1, chunks[2]);
-                        break;
-                    case "div":
-                        div(registers, arg1, chunks[2]);
-                        break;
-                    case "mod":
-                        mod(registers, arg1, chunks[2]);
-                        break;
-                    case "eql":
-                        eql(registers, arg1, chunks[2]);
-                        break;
-                    default:
-                        throw new IllegalArgumentException();
+        List<Integer> ascendingInput = IntStream.rangeClosed(1, 9)
+                .boxed()
+                .collect(Collectors.toList());
+
+        List<Integer> solutionPart2 = new ArrayList<>();
+        pruned = new HashSet<>();
+        trySolve(ascendingInput, 0, solutionPart2, 0, param1, param2, param3, pruned);
+
+        Collections.reverse(solutionPart2);
+        System.out.println("Part2: " + solutionPart2.stream().map(String::valueOf).collect(Collectors.joining()));
+    }
+
+    private static long formula(long z, int w, int param1, int param2, int param3) {
+        boolean xtest = (z % 26 + param2) != w;
+        int x = xtest ? 1 : 0;
+        return x * (w + param3) + (25 * x + 1) * (z / param1);
+    }
+
+    private static boolean trySolve(List<Integer> inputValues, long z, List<Integer> solution, int step,
+                                    int[] param1, int[] param2, int[] param3, Set<Key> pruned) {
+        Key key = new Key(step, z);
+        if (pruned.contains(key)) {
+            return false;
+        }
+        if (step == 14) {
+            if (z == 0) {
+                return true;
+            }
+        } else {
+            for (int w : inputValues) {
+                long prevZ = z;
+                z = formula(z, w, param1[step], param2[step], param3[step]);
+                if (trySolve(inputValues, z, solution, step + 1, param1, param2, param3, pruned)) {
+                    solution.add(w);
+                    return true;
                 }
+                z = prevZ;
             }
-            //success = true;
-            if (registers[2] == 0) {
-                success = true;
-            } else {
-                generateNumber(number);
-            }
-            //System.out.println(toNumber(number));
         }
-        //System.out.println(Arrays.stream(registers).mapToObj(String::valueOf).collect(Collectors.joining(",")));
-        System.out.println("Part1: " + toNumber(number));
-    }
-
-    private static String toNumber(int[] number) {
-        return Arrays.stream(number)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining());
-    }
-
-    private static void generateNumber(int[] number) {
-        int i = number.length - 1;
-        int last = number[i];
-        last--;
-        number[i] = last;
-
-        while (last <= 0 && i > 0) {
-            number[i] = 9;
-            i--;
-            last = number[i];
-            last--;
-            number[i] = last;
-        }
-    }
-
-    private static void eql(long[] registers, String arg1, String arg2) {
-        int mode = 0;
-        int param1 = decodeRegister(arg1);
-        int param2;
-        try {
-            param2 = Integer.parseInt(arg2);
-        } catch (NumberFormatException e) {
-            param2 = decodeRegister(arg2);
-            mode = 1;
-        }
-        if (mode == 0) {
-            registers[param1] = registers[param1] == param2 ? 1 : 0;
-        } else {
-            registers[param1] = registers[param1] == registers[param2] ? 1 : 0;
-        }
-    }
-
-    private static void mod(long[] registers, String arg1, String arg2) {
-        int mode = 0;
-        int param1 = decodeRegister(arg1);
-        int param2;
-        try {
-            param2 = Integer.parseInt(arg2);
-        } catch (NumberFormatException e) {
-            param2 = decodeRegister(arg2);
-            mode = 1;
-        }
-        if (mode == 0) {
-            registers[param1] %= param2;
-        } else {
-            registers[param1] %= registers[param2];
-        }
-    }
-
-    private static void div(long[] registers, String arg1, String arg2) {
-        int mode = 0;
-        int param1 = decodeRegister(arg1);
-        int param2;
-        try {
-            param2 = Integer.parseInt(arg2);
-        } catch (NumberFormatException e) {
-            param2 = decodeRegister(arg2);
-            mode = 1;
-        }
-        if (mode == 0) {
-            registers[param1] /= param2;
-        } else {
-            registers[param1] /= registers[param2];
-        }
-    }
-
-    private static void mul(long[] registers, String arg1, String arg2) {
-        int mode = 0;
-        int param1 = decodeRegister(arg1);
-        int param2;
-        try {
-            param2 = Integer.parseInt(arg2);
-        } catch (NumberFormatException e) {
-            param2 = decodeRegister(arg2);
-            mode = 1;
-        }
-        if (mode == 0) {
-            registers[param1] *= param2;
-        } else {
-            registers[param1] *= registers[param2];
-        }
-    }
-
-    private static void add(long[] registers, String arg1, String arg2) {
-        int mode = 0;
-        int param1 = decodeRegister(arg1);
-        int param2;
-        try {
-            param2 = Integer.parseInt(arg2);
-        } catch (NumberFormatException e) {
-            param2 = decodeRegister(arg2);
-            mode = 1;
-        }
-        if (mode == 0) {
-            registers[param1] += param2;
-        } else {
-            registers[param1] += registers[param2];
-        }
-    }
-
-    private static void inp(long[] registers, int[] number, int inputIndex, String arg1) {
-        registers[decodeRegister(arg1)] = number[inputIndex];
-    }
-
-    private static int decodeRegister(String param) {
-        switch (param) {
-            case "x":
-                return 0;
-            case "y":
-                return 1;
-            case "z":
-                return 2;
-            case "w":
-                return 3;
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
-
-
-    private static List<String> readInput() throws IOException {
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            lines = reader.lines().collect(Collectors.toList());
-        }
-        return lines;
+        pruned.add(key);
+        return false;
     }
 }
 
+class Key {
+    private final int step;
+    private final long z;
 
+    public Key(int step, long z) {
+        this.step = step;
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Key key = (Key) o;
+        return step == key.step &&
+                z == key.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(step, z);
+    }
+}
